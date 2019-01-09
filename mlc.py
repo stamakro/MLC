7053afe34a6a2f2a1512fcf7ad0f495856cb7bf5
Stavros Makrodimitris - EWI <stavrosmakrodi@sanger.hpc.tudelft.nl>
2019-01-09 17:16:02 +0100

    what?

--- mlc.py
+++ mlc.py
@@ -1,4 +1,34 @@
 import numpy as np
 from scipy.optimize import minimize
+
+#only need for testing and calculating performance
 from sklearn.model_selection import KFold
-from sklearn.metrics import roc_auc_score, average_precision_score
+from sklearn.metrics import roc_auc_score
+
+def mlcTrain(Xtrain, ytrain, a, w0=None, maxIter=30):
+
+	[N, f] = Xtrain.shape
+	ytrain = ytrain.reshape(-1,)
+
+	assert ytrain.shape[0] == N
+
+	if w0 is None:
+		w0 = np.ones(f)
+
+
+
+	return w0
+
+
+def mlcPredict(Xtrain, ytrain, Xtest, w):
+
+
+	pass
+
+
+def pccPredict():
+
+
+
+if __name__ == "__main__":
+	print 'Begining demo...'
