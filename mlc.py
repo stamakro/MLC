4a893c45be2de151926ed877723769624b8d03ec
Stavros Makrodimitris - EWI <stavrosmakrodi@sanger.hpc.tudelft.nl>
2019-01-21 15:30:18 +0100

    Finalized code and wrote documentation

--- mlc.py
+++ mlc.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.optimize import minimize
 
 #only needed for testing and calculating performance
-from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
 def tvalueG(w, X, y, a):
@@ -24,15 +24,10 @@ The second is an array of size #samples containing the partial derivatives of th
 to the weights
 
         '''
-		#determine genes with and without GO term (positive and negative genes)
+	#determine genes with and without GO term (positive and negative genes)
         pos = np.where(y)[0]
         neg = np.where(y == 0)[0]
 
-		#subsample the number of genes to reduce computation to at most 100 positives and 100 negatives
-        np.random.seed(2312)
-        maxnr = 100
-        pos = np.random.permutation(pos)[:maxnr]
-        neg = np.random.permutation(neg)[:maxnr]
 
         N1 = pos.shape[0]
         N2 = neg.shape[0]
@@ -150,21 +145,115 @@ An array of size #samples containing the learned sample weights
 	if w0 is None:
 		w0 = np.ones(f)
 
+	#determine genes with and without GO term (positive and negative genes)
+	pos = np.where(ytrain)[0]
+	neg = np.where(ytrain == 0)[0]
+
+	#subsample the number of genes to reduce computation to at most 100 positives and 100 negatives
+	np.random.seed(2312)
+	maxnr = 100
+	pos = np.random.permutation(pos)[:maxnr]
+	neg = np.random.permutation(neg)[:maxnr] 
+
+	ii = np.hstack((pos, neg))
+	ytrain = ytrain[ii]
+	Xtrain = Xtrain[ii]
+
+	#bounds for weights, constraint each w[i] to be non-negative
+	bb = tuple([(0.0, np.inf) for i in xrange(X.shape[1])])
+
+	#minimize cost function
+	solution = minimize(tvalueG, w0, args=(Xtrain, ytrain, a), jac=True, bounds=bb, options={'maxiter': maxIter})
+
+	w = solution.x
+
+	return w
 
 
-	return w0
+def mlcPredict(Xtrain, ytrain, Xtest, w, k):
+	'''
+k-NN classifier with the weighted inner product of MLC as a distance function
+
+Input Arguments:
+Xtrain: training set, a 2-d gene expression array (#genes_train x #samples)
+ytrain: training labels, a label vector of length #genes_train, y[i] == 1 iff gene i is annotated with the Go term/pathway in question and 0 otherwise
+Xtest:  test set, a 2-d gene expression array (#genes_test x #samples)
+w:      an array of size #samples that contains the weights learned by MLC during training
+k:      the number of nearest neighbors to consider
+
+Returns:
+An array of size #genes_test containing the posterior probability that each gene is associated with the label
+
+	'''
+	C = (Xtest * w).dot(Xtrain.T)
+	knns = np.argsort(C, axis=1)[:, -k:]
+
+	ypred = np.sum(ytrain[knns], axis=1) / float(k)
+
+	return ypred
 
 
-def mlcPredict(Xtrain, ytrain, Xtest, w):
+def pccPredict(Xtrain, ytrain, Xtest, k):
+	'''
+For comparison: k-NN classifier with pearson correlation as a distance function
+
+Input Arguments:
+Xtrain: training set, a 2-d gene expression array (#genes_train x #samples)
+ytrain: training labels, a label vector of length #genes_train, y[i] == 1 iff gene i is annotated with the Go term/pathway in question and 0 otherwise
+Xtest:  test set, a 2-d gene expression array (#genes_test x #samples)
+k:      the number of nearest neighbors to consider
+
+Returns:
+An array of size #genes_test containing the posterior probability that each gene is associated with the label
+
+	'''
+	Ntrn = Xtrain.shape[0]
+	Ntst = Xtest.shape[0]
+
+	# a Ntst x Ntrn matrix containing the correlation of all test genes to all training genes
+	C = np.corrcoef(Xtrain, Xtest)[Ntrn:][:, :Ntrn]
+
+	knns = np.argsort(C, axis=1)[:, -k:]
+	ypred = np.sum(ytrain[knns], axis=1) / float(k)
 
 
-	pass
+	return ypred
 
-
-def pccPredict(Xtrain, ytrain, Xtest):
-    pass
 
 
 if __name__ == "__main__":
-	print 'Begining demo...'
-#
+	print('Generating random data...')
+	#generate some random expression data, with 1000 genes and 30 samples
+	X = np.random.rand(1000, 30)
+
+	#generate random labels 0-1
+	y = np.random.randint(0, 2, 1000)
+
+	#split into training and test sets
+	Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.333)
+
+	print('Training MLC...') 
+	#learn weights in training set, setting a to 0.9
+	w = mlcTrain(Xtrain, ytrain, 0.9, w0=None, maxIter=30)
+
+	#set the number of nearest neighbors
+	k = 20
+
+	print('Predicting with MLC...')
+	#use learned weights to make predictions in the test set
+	ypred_mlc = mlcPredict(Xtrain, ytrain, Xtest, w, k)
+
+	print('Predicting with Pearson correlation...')
+	#make prediction using pearson correlation, for comparison
+	ypred_pcc = pccPredict(Xtrain, ytrain, Xtest, k)
+
+
+	#evaluate the two predictors using roc auc
+	auc_mlc = roc_auc_score(ytest, ypred_mlc)
+	auc_pcc = roc_auc_score(ytest, ypred_pcc)
+
+	print ('Performance of PCC:', auc_pcc)
+	print ('Performance of MLC:', auc_mlc)
+
+
+
